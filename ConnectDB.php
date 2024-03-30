707f5cada20bd801ebf4fa791f67cf37b94b84fb
= <=>
2024-03-30 19:53:19 +0530

    Completed

--- ConnectDB.php
+++ ConnectDB.php
@@ -1,14 +1,9 @@
 <?php
 
 $host = "localhost";
-$user = "id21731308_star";
-$password = "@Pranav173";
-$database = "id21731308_placement";
-
-// $host = "localhost";
-// $user = "root";
-// $password = ""; 
-// $database = "placement";
+$user = "root";
+$password = ""; 
+$database = "placement";
 
 $mysqli = new mysqli($host, $user, $password, $database);
 
